ecf5343c74000395239bfc3511f4e224271df505
RaulCas <raul.castanera@unavarra.es>
2018-03-28 15:44:09 +0200

    Update run_diversity.sh

--- scripts/run_diversity.sh
+++ scripts/run_diversity.sh
@@ -11,20 +11,18 @@ bash merge_OTU_tables.sh
 
 
 # Alpha diversity
-$usearch -alpha_div otutab.txt -output alpha.txt
-
+usearch -alpha_div otutab.txt -output alpha.txt
 
 # Beta diversity
 
 mkdir beta/
-$usearch -beta_div otutab.txt -tree otus.tree -filename_prefix beta/
+usearch -beta_div otutab.txt -tree otus.tree -filename_prefix beta/
 
 # Rarefaction
-$usearch -alpha_div_rare otutab.txt -output rare.txt
+usearch -alpha_div_rare otutab.txt -output rare.txt
 
 # Predict taxonomy
-$usearch -sintax otus.fa -db ../data/rdp_16s_v16.fa -strand both \
-  -tabbedout sintax.txt -sintax_cutoff 0.8
+usearch -sintax v3_4_otus.fa -db /rhome/rcastanera/bigdata/Kiel/Database/rdp_16s.udb -strand both -tabbedout v3_4_otus_sintax.txt -sintax_cutoff 0.8
 
 # Taxonomy summary reports
 $usearch -sintax_summary sintax.txt -otutabin otutab.txt -rank g -output genus_summary.txt

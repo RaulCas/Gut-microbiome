02a1006710b6f0d4e65a26f9ddc4c83472ed4d93
RaulCas <raul.castanera@unavarra.es>
2018-03-28 15:15:32 +0200

    Update run_OTU_table.sh

--- run_OTU_table.sh
+++ run_OTU_table.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 
 # ================================
-# continue with single samples
+# Run this step in every sample
 # ================================
 
 
-# STEP 1: Create OTU Tables: #### """ Repeat this Step for all samples """ ####
+# STEP 1: Create OTU Tables: 
 
-usearch -otutab /bigdata/castaneralab/rcastanera/Kiel/G09001/out.extendedFrags.fastq -otus otus.fa -otutabout G09001_otutab.txt -mapout G09001_map.txt
+usearch -otutab /bigdata/castaneralab/rcastanera/Kiel/G09001/G09002.all.fasta -otus otus.fa -otutabout G09001_otutab.txt -mapout G09001_map.txt
 
-# STEP 2: Normalize to 10,000 reads #### """ Repeat this Step for all samples """ ####
+# STEP 2: Normalize to 10,000 reads 
 
 usearch -otutab_norm G09001_otutab.txt -sample_size 10000 -output G09001_norm.txt
 

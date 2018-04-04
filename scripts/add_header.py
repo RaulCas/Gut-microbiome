30048d19b4f068f17d9c6984ebd3226f5be7d55b
RaulCas <raul.castanera@unavarra.es>
2018-04-04 14:23:48 +0200

    Create add_header.py

---
+++ scripts/add_header.py
@@ -0,0 +1,16 @@
+#==============================================================================================================
+#  This script renames the header in single OTU Tables, prior to running merge_otu_tables.py script from qiime
+#  Uses information of the filename (split by '-') to add it in the second column
+#==============================================================================================================
+
+import os
+
+for filename in os.listdir("."):
+	if filename.endswith(".txt_norm.txt"):
+		working=open(filename, 'r')
+		outfile=open(filename+"_renamed", 'w')
+		newfilename=str(filename.split('-')[0])
+		newheader="OTU ID "+str(newfilename)
+		outfile.write(newheader+'\n')
+		for line in working[1:]:
+			outfile.write(line)
